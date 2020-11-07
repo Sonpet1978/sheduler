1aaf0dbf9f6ba3662fcf26a66fb6dd86d0082d87
Sonya Korolev Shapiro <sonpet1978@gmail.com>
2020-11-06 22:48:31 -0500

    Add bug fixese

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,21 +2,75 @@ import React from "react";
 
 import "components/Application.scss";
 
-export default function Application(props) {
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
+import useApplicationData from "hooks/useApplicationData";
 
+import "components/Application.scss";
+
+import DayList from "components/DayList";
+import Appointment from "./Appointment";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay} from "helpers/selectors";
+
+
+export default function Application(props) {
+ 
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelAppointment
+  } = useApplicationData();
+
+ 
+    const appointments = getAppointmentsForDay(state, state.day);
+
+    const interviewersForDay = getInterviewersForDay(state, state.day);
+
+
+
+let schedule=[];
+if (appointments && appointments[0] && state.interviewers){
+    schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
   
+return (
+    <Appointment
+    key={appointment.id}
+    id={appointment.id}
+    time={appointment.time}
+    interviewers={interviewersForDay}
+    interview={interview}
+    bookInterview={bookInterview}
+    cancelAppointment={cancelAppointment}
+
+  />
+  );
+  })
+}
   return (
     <main className="layout">
       <section className="sidebar">
-        {
-          
-        
-        }
+      
+          <img
+          className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+         <DayList
+         days={state.days}
+         day={state.day}
+         setDay={setDay}
+         />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"
+        />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      <section className="schedule"> {schedule}
+      <Appointment id="last" time="5pm" />
+ 
       </section>
     </main>
   );
-  
 }
---
+++ src/components/InterviewerList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import InterviewerListItem from "components/InterviewerListItem";
+
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+
+const interviewers = props.interviewers.map(interviewer => {
+return (
+    <InterviewerListItem
+    key={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected={interviewer.id === props.value}
+    // selected={interviewer.id === props.interviewer}
+    setInterviewer={event => props.onChange(interviewer.id)}
+    // setInterviewer={event => props.setInterviewer(interviewer.id)}
+    />
+); 
+});
+
+return (
+<section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list"> 
+       {interviewers}
+  </ul>
+</section>
+)};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
